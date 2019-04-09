192f321ecd19dba30dfced4e09990ecec5081b7b
Petty <pettyferlove@live.cn>
2019-04-09 13:33:52 +0800

    Initial commit

---
+++ rich-config-service/src/main/java/com/github/rich/config/RichConfigServiceApplication.java
@@ -0,0 +1,36 @@
+package com.github.rich.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 微服务云配置中心
+ * @author Petty
+ */
+@Slf4j
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RichConfigServiceApplication {
+
+    /**
+     * 负载均衡配置
+     * @return RestTemplate
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichConfigServiceApplication.class, args);
+    }
+
+}
