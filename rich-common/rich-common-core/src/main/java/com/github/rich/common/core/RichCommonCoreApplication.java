a22f037daf787b8453f49601677569e8de38e3cf
Petty <pettyferlove@live.cn>
2019-04-16 10:43:56 +0800

    init

---
+++ rich-common/rich-common-core/src/main/java/com/github/rich/common/core/RichCommonCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.rich.common.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichCommonCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichCommonCoreApplication.class, args);
+    }
+
+}
