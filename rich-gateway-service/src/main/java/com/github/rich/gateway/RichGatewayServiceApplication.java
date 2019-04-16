b15e838dcf652792ae7fecf6007b16606857e9b2
Petty <pettyferlove@live.cn>
2019-04-16 09:58:38 +0800

    init

---
+++ rich-gateway-service/src/main/java/com/github/rich/gateway/RichGatewayServiceApplication.java
@@ -0,0 +1,33 @@
+package com.github.rich.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Petty
+ */
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RichGatewayServiceApplication {
+
+	/**
+	 * 负载均衡配置
+	 * @return RestTemplate
+	 */
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ rich-gateway-service/src/test/java/com/github/rich/gateway/RichGatewayServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.rich.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RichGatewayServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
