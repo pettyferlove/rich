9ef16452f3506455291301fc564a331300d41afe
Petty <pettyferlove@live.cn>
2019-04-19 11:46:20 +0800

    init

---
+++ rich-common/rich-common-core/src/main/java/com/github/rich/common/core/aop/ControllerAop.java
@@ -0,0 +1,72 @@
+package com.github.rich.common.core.aop;
+
+import com.github.rich.common.core.exception.BaseException;
+import com.github.rich.common.core.exception.BaseRuntimeException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Objects;
+
+
+/**
+ * 通过Aop对Controller返回异常进行包装
+ *
+ * @author Petty
+ * @date 2018年3月1日
+ */
+@Aspect
+@Order(10)
+@Component
+@Slf4j
+public class ControllerAop {
+
+    @Pointcut("execution(public com.github.rich.common.core.model.R *(..))")
+    public void pointCutResponse() {
+    }
+
+    /**
+     * 处理切点
+     *
+     * @param pjp ProceedingJoinPoint
+     * @return Object
+     */
+    @Around("pointCutResponse()")
+    public Object methodAroundHandler(ProceedingJoinPoint pjp) {
+        return methodHandler(pjp);
+    }
+
+    private Object methodHandler(ProceedingJoinPoint pjp) {
+        long startTime = System.currentTimeMillis();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+        log.info("CLASS_METHOD : " + pjp.getSignature().getDeclaringTypeName() + "." + pjp.getSignature().getName());
+        log.info("ARGS : " + Arrays.toString(pjp.getArgs()));
+        Object result;
+        try {
+            result = pjp.proceed();
+            log.info(pjp.getSignature() + "use time:" + (System.currentTimeMillis() - startTime));
+        } catch (Throwable e) {
+            log.error("Error：", e);
+            if(e instanceof BaseRuntimeException) {
+                throw new BaseRuntimeException(e.getMessage(),((BaseRuntimeException) e).getStatus());
+            }else if (e instanceof BaseException) {
+                throw new BaseRuntimeException(e.getMessage(),((BaseException) e).getStatus());
+            }else {
+                throw new BaseRuntimeException(e.getMessage());
+            }
+        }
+        return result;
+    }
+}
--- rich-module/rich-base-info-service/src/main/java/com/github/rich/base/controller/RoleController.java
+++ rich-module/rich-base-info-service/src/main/java/com/github/rich/base/controller/RoleController.java
@@ -1,7 +1,27 @@
 package com.github.rich.base.controller;
 
+import com.github.rich.base.service.ISystemRoleService;
+import com.github.rich.common.core.model.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author Petty
  */
+@RestController
+@RequestMapping("/role")
 public class RoleController {
+    private final ISystemRoleService systemRoleService;
+
+    public RoleController(ISystemRoleService systemRoleService) {
+        this.systemRoleService = systemRoleService;
+    }
+
+    @GetMapping("/find")
+    public R<List> find(){
+        return new R<>(systemRoleService.list());
+    }
 }
