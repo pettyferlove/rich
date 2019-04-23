56eb7de4ad04f5d521edf7f1d696453307ef9b74
Petty <pettyferlove@live.cn>
2019-04-23 15:38:19 +0800

    修改Tomcat容器为Undertow

--- rich-module/rich-demo-service/src/main/java/com/github/rich/demo/controller/DemoController.java
+++ rich-module/rich-demo-service/src/main/java/com/github/rich/demo/controller/DemoController.java
@@ -1,13 +1,9 @@
 package com.github.rich.demo.controller;
 
-import com.github.rich.base.dto.User;
-import com.github.rich.base.feign.RemoteRoleService;
 import com.github.rich.base.feign.RemoteUserService;
-import com.github.rich.common.core.model.R;
 import com.github.rich.security.annotation.InnerServiceSecurity;
 import com.github.rich.security.service.impl.UserDetailsImpl;
 import com.github.rich.security.utils.SecurityUtil;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +18,9 @@ import java.util.List;
 @RequestMapping("/demo")
 public class DemoController {
 
-    private final RemoteRoleService remoteRoleService;
     private final RemoteUserService remoteUserService;
 
-    public DemoController(RemoteRoleService remoteRoleService, RemoteUserService remoteUserService) {
-        this.remoteRoleService = remoteRoleService;
+    public DemoController(RemoteUserService remoteUserService) {
         this.remoteUserService = remoteUserService;
     }
 
@@ -38,7 +32,5 @@ public class DemoController {
         System.out.println(user);
         System.out.println(roles);
         System.out.println("Success");
-        R<List> admin = remoteRoleService.find();
-        System.out.println(admin);
     }
 }
