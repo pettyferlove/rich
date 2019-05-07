09f5ec138b9809ac8048a624dae94b83cf889016
Petty <pettyferlove@live.cn>
2019-05-07 15:32:11 +0800

    Swagger文档修改

--- rich-tx-manager-service/src/main/java/com/github/rich/txmanager/RichTxManagerServiceApplication.java
+++ rich-tx-manager-service/src/main/java/com/github/rich/txmanager/RichTxManagerServiceApplication.java
@@ -2,13 +2,13 @@ package com.github.rich.txmanager;
 
 import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * @author Petty
  */
 @EnableTransactionManagerServer
-@SpringBootApplication
+@SpringCloudApplication
 public class RichTxManagerServiceApplication {
 
     public static void main(String[] args) {
