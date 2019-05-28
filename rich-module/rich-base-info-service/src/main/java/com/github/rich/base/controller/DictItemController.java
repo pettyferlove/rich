0692ed06301df4e76a04ae13c1c3e53388354a96
Petty <pettyferlove@live.cn>
2019-05-28 18:53:43 +0800

    字典接口

--- rich-module/rich-base-info-service/src/main/java/com/github/rich/base/controller/DictItemController.java
+++ rich-module/rich-base-info-service/src/main/java/com/github/rich/base/controller/DictItemController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -40,48 +41,48 @@ public class DictItemController {
             @ApiImplicitParam(paramType = "path", name = "code", value = "Code", dataTypeClass = String.class)
     })
     @GetMapping("/{code}")
-    public R<SystemDictItem> get(@PathVariable String code){
+    public R<SystemDictItem> get(@PathVariable String code) {
         return new R<>(systemDictItemService.get(code));
     }
 
-    @ApiOperation(value = "创建字典项", notes = "需要管理员权限",authorizations = @Authorization(value = "oauth2"))
+    @ApiOperation(value = "创建字典项", notes = "需要管理员权限", authorizations = @Authorization(value = "oauth2"))
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "object", name = "dictItem", value = "DictItem", dataTypeClass = SystemDictItem.class)
     })
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public R<String> create(SystemDictItem systemDictItem){
+    public R<String> create(SystemDictItem systemDictItem) {
         return new R<>(systemDictItemService.create(systemDictItem));
     }
 
-    @ApiOperation(value = "更新字典项", notes = "需要管理员权限",authorizations = @Authorization(value = "oauth2"))
+    @ApiOperation(value = "更新字典项", notes = "需要管理员权限", authorizations = @Authorization(value = "oauth2"))
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "object", name = "dictItem", value = "DictItem", dataTypeClass = SystemDictItem.class)
     })
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping
-    public R<Boolean> update(SystemDictItem systemDictItem){
+    public R<Boolean> update(SystemDictItem systemDictItem) {
         return new R<>(systemDictItemService.update(systemDictItem));
     }
 
-    @ApiOperation(value = "删除字典项", notes = "需要管理员权限",authorizations = @Authorization(value = "oauth2"))
+    @ApiOperation(value = "删除字典项", notes = "需要管理员权限", authorizations = @Authorization(value = "oauth2"))
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "path", name = "code", value = "Code", dataTypeClass = String.class)
     })
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{code}")
-    public R<Boolean> delete(@PathVariable String code){
+    public R<Boolean> delete(@PathVariable String code) {
         return new R<>(systemDictItemService.delete(code));
     }
 
-    @ApiOperation(value = "批量删除字典项", notes = "需要管理员权限",authorizations = @Authorization(value = "oauth2"))
+    @ApiOperation(value = "批量删除字典项", notes = "需要管理员权限", authorizations = @Authorization(value = "oauth2"))
     @ApiImplicitParams({
-            @ApiImplicitParam(paramType = "query", name = "codes", value = "Codes", dataTypeClass = List.class)
+            @ApiImplicitParam(paramType = "query", name = "codes", value = "Codes", dataTypeClass = String.class)
     })
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/delete/batch")
     @Deprecated
-    public R<Boolean> deleteByCodes(List<String> codes){
-        return new R<>(systemDictItemService.deleteByCodes(codes));
+    public R<Boolean> deleteByCodes(String codes) {
+        return new R<>(systemDictItemService.deleteByCodes(Arrays.asList(codes.split(","))));
     }
 }
