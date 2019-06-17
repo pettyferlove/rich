c4ecf8833ddc08d8282830824fdf356023a550ca
Petty <pettyferlove@live.cn>
2019-06-17 21:49:01 +0800

    init

--- rich-zipkin-service/src/main/java/com/github/rich/zipkin/RichZipkinServiceApplication.java
+++ rich-zipkin-service/src/main/java/com/github/rich/zipkin/RichZipkinServiceApplication.java
@@ -3,10 +3,12 @@ package com.github.rich.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 /**
  * @author Petty
  */
+@EnableZipkinServer
 @SpringCloudApplication
 public class RichZipkinServiceApplication {
 
