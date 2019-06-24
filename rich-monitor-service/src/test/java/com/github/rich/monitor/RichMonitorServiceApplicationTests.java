2c7f4c4e30736019461364e0ee51893b08d445ad
Petty <pettyferlove@live.cn>
2019-06-24 17:16:58 +0800

    init

--- rich-monitor-service/src/test/java/com/github/rich/monitor/RichMonitorServiceApplicationTests.java
+++ rich-monitor-service/src/test/java/com/github/rich/monitor/RichMonitorServiceApplicationTests.java
@@ -1,11 +1,7 @@
 package com.github.rich.monitor;
 
-import com.github.rich.common.core.constants.RabbitMqQueueConstant;
-import com.github.rich.common.core.dto.Message;
-import com.github.rich.common.core.service.IMessageSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,12 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RichMonitorServiceApplicationTests {
 
-    @Autowired
-    private IMessageSender messageSender;
-
     @Test
     public void contextLoads() {
-        messageSender.send(RabbitMqQueueConstant.SERVICE_STATUS_CHANGE_QUEUE,new Message("1","1","1","1"));
+
     }
 
 }
