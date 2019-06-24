a3de9b0e5dd9cecb328cd7d8c70160e3134b9280
Petty <pettyferlove@live.cn>
2019-06-24 14:54:41 +0800

    init

--- rich-common/rich-common-core/src/main/java/com/github/rich/common/core/annotation/EnableSwaggerDoc.java
+++ rich-common/rich-common-core/src/main/java/com/github/rich/common/core/annotation/EnableSwaggerDoc.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
+/**
+ * @author Petty
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
