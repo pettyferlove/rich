489c2a8ab6e116cd3fedb64b573e83175101e22d
Petty <pettyferlove@live.cn>
2019-06-27 09:34:16 +0800

    WebSocket消息推送

---
+++ rich-common/rich-common-core/src/main/java/com/github/rich/common/core/dto/message/UserGeneralMessage.java
@@ -0,0 +1,13 @@
+package com.github.rich.common.core.dto.message;
+
+import com.github.rich.common.core.dto.Message;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class UserGeneralMessage extends Message {
+
+}
