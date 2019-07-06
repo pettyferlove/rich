65c30d9c87835c0f95341468375b1700af1b346f
Petty <pettyferlove@live.cn>
2019-07-06 13:40:31 +0800

    加密方式更改

--- rich-monitor-service/src/main/java/com/github/rich/monitor/RichMonitorServiceApplication.java
+++ rich-monitor-service/src/main/java/com/github/rich/monitor/RichMonitorServiceApplication.java
@@ -3,7 +3,6 @@ package com.github.rich.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -16,6 +15,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @Slf4j
 @EnableAdminServer
 @EnableHystrix
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @EnableTurbine
 @SpringCloudApplication
