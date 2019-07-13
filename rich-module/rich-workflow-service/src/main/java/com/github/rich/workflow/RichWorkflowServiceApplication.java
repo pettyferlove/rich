94417266e053d90c0e34e46fedc257ad9ca95b88
Petty <pettyferlove@live.cn>
2019-07-13 09:58:45 +0800

    增加Workflow服务---待开发

---
+++ rich-module/rich-workflow-service/src/main/java/com/github/rich/workflow/RichWorkflowServiceApplication.java
@@ -0,0 +1,32 @@
+package com.github.rich.workflow;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Petty
+ */
+@Slf4j
+@SpringCloudApplication
+public class RichWorkflowServiceApplication {
+
+    /**
+     * 负载均衡配置
+     *
+     * @return RestTemplate
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichWorkflowServiceApplication.class, args);
+    }
+
+}
