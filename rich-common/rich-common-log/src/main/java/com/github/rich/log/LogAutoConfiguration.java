745b7f0a4592005d8c3475c97501b41ba2a70968
Alex Pettyfer <pettyferlove@live.cn>
2019-11-30 12:37:25 +0800

    移除响应体内部的状态码，与http状态码保持一致

--- rich-common/rich-common-log/src/main/java/com/github/rich/log/LogAutoConfiguration.java
+++ rich-common/rich-common-log/src/main/java/com/github/rich/log/LogAutoConfiguration.java
@@ -1,19 +1,3 @@
-/*
- *  Copyright (c) 2019-2020, 冷冷 (wangiegie@gmail.com).
- *  <p>
- *  Licensed under the GNU Lesser General Public License 3.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  <p>
- * https://www.gnu.org/licenses/lgpl.html
- *  <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.rich.log;
 
 import com.github.rich.log.aop.UserLogAop;
@@ -26,9 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
- * @author lengleng
- * @date 2019/2/1
  * 日志自动配置
+ * @author Petty
+ * @date 2019-11-26
  */
 @EnableAsync
 @Configuration
