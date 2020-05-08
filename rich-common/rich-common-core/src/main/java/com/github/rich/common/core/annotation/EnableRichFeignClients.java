b2ee4d05d13acb5dbbf5833cf1cc0fd9a39ac927
Alex Pettyfer <pettyferlove@live.cn>
2020-05-08 15:51:43 +0800

    Feign接口规范

--- rich-common/rich-common-core/src/main/java/com/github/rich/common/core/annotation/EnableRichFeignClients.java
+++ rich-common/rich-common-core/src/main/java/com/github/rich/common/core/annotation/EnableRichFeignClients.java
@@ -13,7 +13,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.github.rich")
 public @interface EnableRichFeignClients {
 
     /**
--- rich-common/rich-common-security/src/main/java/com/github/rich/security/annotation/EnableRichResourceServer.java
+++ rich-common/rich-common-security/src/main/java/com/github/rich/security/annotation/EnableRichResourceServer.java
@@ -2,6 +2,7 @@ package com.github.rich.security.annotation;
 
 import com.github.rich.security.component.RichResourceServerAutoConfiguration;
 import com.github.rich.security.component.RichResourceServerConfigurerAdapter;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,9 +12,10 @@ import java.lang.annotation.*;
 /**
  * 初始化资源服务
  * 该注解将会把服务变成OAuth2服务系统中的资源服务，会忽略Spring Security自身部分功能
+ *
  * @author Petty
- * @since 如果应用需要自己实现登录等操作，则不要使用该注解
  * @version 1.0.0
+ * @since 如果应用需要自己实现登录等操作，则不要使用该注解
  */
 @Documented
 @Inherited
@@ -21,6 +23,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableFeignClients(clients = {com.github.rich.base.feign.UserServiceFeignClient.class})
 @Import({RichResourceServerAutoConfiguration.class, RichResourceServerConfigurerAdapter.class})
 public @interface EnableRichResourceServer {
 }
